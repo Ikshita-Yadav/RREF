625d872c343d816d0ff22ce4bc005f149540cd13
Ikshita-Yadav <59580348+Ikshita-Yadav@users.noreply.github.com>
2023-01-09 15:30:50 -0500

    working algorithm for rref but the input from the tesseract is wrong

--- rref.py
+++ rref.py
@@ -1,12 +1,12 @@
 from PIL import Image
 from pytesseract import pytesseract
-from numpy import array
+from sympy import *
 
 # Define path to tessaract.exe
 path_to_tesseract = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
 # Define path to image
-path_to_image = 'sample_matrix.JPG'
+path_to_image = 'images/sample_matrix.JPG'
 
 # Point tessaract_cmd to tessaract.exe
 pytesseract.tesseract_cmd = path_to_tesseract
@@ -14,11 +14,57 @@ pytesseract.tesseract_cmd = path_to_tesseract
 # Open image with PIL
 img = Image.open(path_to_image)
 
-arr = array(img)
-
 # Extract text from image
 text = pytesseract.image_to_string(img)
 
 print(text)
-print("ARRAY")
-print(arr)
+
+# Split the text into rows and columns
+rows = text.split('\n')
+matrix = [row.split() for row in rows]
+
+# Convert the strings in the matrix to integers
+matrix = [[int(cell) for cell in row] for row in matrix]
+
+
+# Print the matrix
+print(matrix)
+
+# RREF CALC
+M = Matrix(matrix)
+print("Matrix : {} ".format(M))
+
+# Use sympy.rref() method
+M_rref = M.rref()
+
+print("The Row echelon form of matrix M and the pivot columns : {}".format(M_rref))
+
+# import pytesseract
+# from PIL import Image
+
+# # path_to_tesseract = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+
+# # Define path to tessaract.exe
+# path_to_tesseract = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+
+# # Define path to image
+# path_to_image = 'images/sample_matrix.JPG'
+
+# # Point tessaract_cmd to tessaract.exe
+# pytesseract.tesseract_cmd = path_to_tesseract
+
+# # Open image with PIL
+# img = Image.open(path_to_image)
+
+# # Extract text from image
+# text = pytesseract.image_to_string(img)
+
+# # Split the text into rows and columns
+# rows = text.split('\n')
+# matrix = [row.split() for row in rows]
+
+# # Convert the strings in the matrix to integers
+# matrix = [[int(cell) for cell in row] for row in matrix]
+
+# # Print the matrix
+# print(matrix)
